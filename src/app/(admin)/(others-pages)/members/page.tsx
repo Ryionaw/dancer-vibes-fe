9fa2e156d2621145788420429d1e2b3193bc1bcc
Ryionaw <ryanarielwinata@gmail.com>
2025-04-02 04:02:03 +0700

    Change Table Title

--- src/app/(admin)/(others-pages)/members/page.tsx
+++ src/app/(admin)/(others-pages)/members/page.tsx
@@ -13,9 +13,9 @@ export default function Profile() {
     <div>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
         <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
-          Woi
+          Members
         </h3>
-          <MemberTable />
+        <MemberTable />
       </div>
     </div>
   );
